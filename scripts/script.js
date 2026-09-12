5155d4f1d2fa644c969e79cbde0a14ee7fb11f5a
gabbyte <igorgabriel.png@gmail.com>
2026-09-12 17:09:22 -0300

    Adicionando excluir e concluir

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,22 @@ adicionar.addEventListener('click', () => {
         nome: nomeHabito.value,
         concluido: false
     }
-    novoHabito.push()
+})
 
-    const linha = document.createElement('')
+
+listaHabitos.addEventListener('click', (event) => {                           // sempre que clicar em algo da lista, passa um evento
+    const marcar = event.target.closest('.novo-habito, .novo-habito-checado') // 'habito' recebe aonde foi o click, independente do item/div/elemento da classe (pelo closest)
+    const excluir = event.target.closest('.excluir-habito')                   // 'excluir' recebe quando excluir ou seus filhos forem clicados
+    
+    if (!marcar) return                                                       // se marcar nao existir
+    const index = Number(marcar.dataset.index)                                // 'index' recebe o data-index de marcar(click na classe)
+    
+    if (excluir) {                                                            // se chamar 'excluir'
+        habitos = habitos.filter((x, indice) => indice !== index)             // cria um novo array filtrando, se o indice for diferente do index, poe na lista(array)
+
+    } else if (marcar){                                                       // se chamar 'marcar'
+        habitos[index].concluido = !habitos[index].concluido                  // se o habito com aquele index, for true ele fica false, se for false fica true
+    }
+    mostrarNaTela()
+    atualizarContadores()
 })
